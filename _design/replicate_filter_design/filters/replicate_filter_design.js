d48c33e47e6575e8fd7859ebeb7338e80887ff58
SAPikachu <i@sapika.ch>
2020-02-11 18:19:39 +0000

    Auto commit on Tue Feb 11 18:19:39 UTC 2020

--- _design/replicate_filter_design/filters/replicate_filter_design.js
+++ _design/replicate_filter_design/filters/replicate_filter_design.js
@@ -1,3 +1,3 @@
 function(doc, req) {
-  return doc._id.indexOf("_design/") === 0 && doc._id !== "_design/default" && doc._id !== "_design/default_test";
+  return (doc._id.indexOf("_design/") === 0 && doc._id !== "_design/default" && doc._id !== "_design/default_test") || doc._id.indexOf("data") === 0;
 }
