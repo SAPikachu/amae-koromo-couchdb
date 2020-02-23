d80231581fe5d4be044c6da6f926beb8e4bbb809
SAPikachu <i@sapika.ch>
2020-02-23 18:25:12 +0000

    Auto commit on Sun Feb 23 18:25:12 UTC 2020

--- _design/renderers/views/lib/player_stats.js
+++ _design/renderers/views/lib/player_stats.js
@@ -7,13 +7,20 @@ exports.func = function(value) {
 		level: value.level && convert_level(value.level),
 		max_level: value.max_level && convert_level(value.max_level),
 	};
-	result.rank_rates = ranks.map(function(x) { return x / result.count; });
+	if (Math.floor(result.level.id / 10000) === 2) {
+		ranks = ranks.slice(0, 3);
+	}
+	result.rank_rates = ranks.map(function(x) {
+		return x / result.count;
+	});
 	if (value.score_accum) {
-		result.rank_avg_score = value.score_accum.map(function(x, i) {
+		result.rank_avg_score = value.score_accum.slice(0, ranks.length).map(function(x, i) {
 			return Math.round(x / ranks[i] * 100);
 		});
 	}
-	result.avg_rank = sum(ranks.map(function(x, index) { return x * (index + 1); })) / result.count;
+	result.avg_rank = sum(ranks.map(function(x, index) {
+		return x * (index + 1);
+	})) / result.count;
 	result.negative_rate = value.accum[4] / result.count;
 	return result;
 }
