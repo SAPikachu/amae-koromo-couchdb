5ec25444a9a777bb4a65a59dfb3a8142474f52e8
SAPikachu <i@sapika.ch>
2022-03-27 21:41:56 +0000

    Auto commit on Sun 27 Mar 2022 09:41:56 PM UTC

--- _design/replicate_filter_design_slim/filters/replicate_filter_design.js
+++ _design/replicate_filter_design_slim/filters/replicate_filter_design.js
@@ -1,3 +1,13 @@
 function(doc, req) {
-  return (doc._id.indexOf("_design/") === 0 && doc._id !== "_design/validate_doc_update" && doc._id !== "_design/player_stats_2" && doc._id !== "_design/updated_players" && doc._id !== "_design/player_extended_stats") || doc._id.indexOf("data") === 0;
+  return (doc._id.indexOf("_design/") === 0 &&
+					doc._id !== "_design/validate_doc_update" &&
+					doc._id !== "_design/player_stats_2" &&
+					doc._id !== "_design/updated_players" &&
+					doc._id !== "_design/player_extended_stats" &&
+					doc._id !== "_design/rank_rate_by_seat" &&
+					doc._id !== "_design/highlight_games" &&
+					doc._id !== "_design/fan_stats" &&
+					doc._id !== "_design/default" &&
+					doc._id !== "_design/nicknames") ||
+		doc._id.indexOf("data") === 0;
 }
